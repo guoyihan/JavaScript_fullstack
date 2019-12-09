8140795b84983a1fae5a1326579ca51747e0f195
735940104@qq.com <735940104@qq.com>
2019-12-09 21:08:31 +0800

    pages

---
+++ vue-learn/vue-music/src/components/pages/search.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <v-search-box @query="onQueryChange" ref="searchBox"></v-search-box>  <!-- 子组件标签里添加ref可以直接调用methods里的方法, @query 是调用 子组件里$emit抛出的方法 -->
+    </div>
+    <!-- 热门搜索和搜索历史 -->
+    <div class="shortcut-wrapper" v-show="!query">
+      <v-scroll class="shortcut" :data="shortcut" ref="shortcut">
+        <div>
+          <!-- 热门搜索 -->
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item" v-for="(item, index) in hotKey" :key="index">
+                <span>{{item.first}}</span>
+              </li>
+            </ul>
+          </div>
+          <!-- 搜索历史 -->
+          <div class="search-history">
+            <h1 class="title">
+              <span class="text">搜索历史</span>
+              <span class="clear">
+                <i class="icon" @click="clearSearchHistory">&#xe612;</i>
+              </span>
+            </h1>
+            <!-- 搜索历史列表 -->
+            <v-search-List :searches="searchHistory" @select="saveSearch" @delete="deleteSearchHistory"></v-search-List>
+          </div>
+        </div>
+      </v-scroll>
+    </div>
+    <!-- result -->
+    <div class="search-result"  v-show="query">
+      <v-suggest :query="query" @select="saveSearch" @listScroll="blurInput" ref="suggest"></v-suggest>
+    </div>
+  </div>
+</template>
+
+<script>
+import searchBox from '@/components/searchBox'
+import scroll from '@/components/scroll'
+import api from '@/api/index.js'
+import searchList from '@/components/searchList'
+import { mapGetters } from 'vuex'
+import { searchMixin } from '@/common/mixin'
+import suggest from '@/components/suggest'
+export default {
+  data () {
+    return {
+      shortcut: [],
+      hotKey: []
+    }
+  },
+  components: {
+    'v-search-box': searchBox,
+    'v-scroll': scroll,
+    'v-search-List': searchList,
+    'v-suggest': suggest
+  },
+  mixins: [searchMixin],  // 扩展 // 相当于extend
+  methods: {
+    _getHotKey () {
+      api.HotSearchKey().then((res) => {
+        if (res.code == 200) {
+          this.hotKey = res.result.hots.slice(0, 10)
+        }
+         console.log(res)
+      })
+    }
+  },
+  created () {
+    this._getHotKey ()
+  },
+  computed: {
+    ...mapGetters([
+      'searchHistory'
+    ])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../../assets/css/function"
+.search
+  overflow hidden
+  &-box-wrapper
+    margin px2rem(40px)
+  .shortcut-wrapper
+    position fixed
+    top px2rem(360px)
+    bottom 0
+    width 100%
+    .shortcut
+      height 100%
+      overflow hidden
+      .hot-key
+        margin 0 px2rem(40px) px2rem(40px) px2rem(40px)
+        .title
+          margin-bottom px2rem(40px)
+          font-size 14px
+          color hsla(0, 0%, 100%, 0.5)
+        .item
+          display inline-block
+          padding px2rem(10px) px2rem(20px)
+          margin 0 px2rem(20px) px2rem(20px) 0
+          border-radius 6px
+          font-size 14px
+          color hsla(0, 0%, 100%, 0.3)
+          background #2f3054
+      .search-history
+        position relative
+        margin 0 px2rem(40px)
+        .title
+          display flex
+          align-items center
+          height px2rem(80px)
+          font-size 14px
+          color hsla(0, 0%, 100%, 0.5)
+          .text
+            flex 1
+          .clear
+            .icon
+              font-size 18px
+              color hsla(0, 0%, 100%, 0.3)
+  .search-result
+    position fixed
+    width 100%
+    bottom 0
+    top px2rem(360px)
+</style>
