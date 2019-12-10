0f2ade0f7dff11f84a740cbf7f85e3f932f39bd8
735940104@qq.com <735940104@qq.com>
2019-12-10 23:13:17 +0800

    components

---
+++ vue-learn/vue-components/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import A from '@/components/pages/A'
+import Parent from '@/components/views/parent'
+import Show from '@/components/form/show'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/A',
+      name: 'A',
+      component: A
+    },
+    {
+      path: '/Parent',
+      name: 'Parent',
+      component: Parent
+    },
+    {
+      path: '/Show',
+      name: 'Show',
+      component: Show
+    }
+  ]
+})
