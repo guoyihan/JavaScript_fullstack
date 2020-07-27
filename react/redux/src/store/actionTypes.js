d29a79e5409e6d46c956af8cfa429721673a3141
735940104@qq.com <735940104@qq.com>
2020-07-27 15:06:22 +0800

    up

---
+++ react/redux/src/store/actionTypes.js
@@ -0,0 +1,3 @@
+export const CHANG_INPUT_VALUE = 'chang_input_value'
+export const ADD_TO_ITEM = 'add_todo_item'
+export const DELETE_TODO_ITEM = 'delete_todo_item'
--- react/redux/src/store/reducer.js
+++ react/redux/src/store/reducer.js
@@ -1,7 +1,33 @@
+import {CHANG_INPUT_VALUE, ADD_TO_ITEM, DELETE_TODO_ITEM} from './actionTypes'
+
+
 const defaultState = {
-    inputValue: '123',
-    list: [1,2]
+    inputValue: '',
+    list: []
 }
+
+//state 指的是store里面上一次存储的数据
+//action 用户传过来的那句话
 export default (state = defaultState, action) => {
+    if(action.type === CHANG_INPUT_VALUE) {
+        const newState = JSON.parse(JSON.stringify(state))  //用JSON深拷贝
+        newState.inputValue = action.value
+        return newState// reducer 有一个限制  可以接受state, 但是绝对不能修改state
+    }
+
+
+    if(action.type === ADD_TO_ITEM) {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
+
+    if(action.type === DELETE_TODO_ITEM) {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.index, 1)
+        return newState
+    }
+    console.log(state, action)
     return  state
 }
